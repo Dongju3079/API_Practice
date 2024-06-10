4760ef9926de3a72e4346ccba6d2b15a56d752b3
dongju <131073398+Dongjuice@users.noreply.github.com>
2024-06-10 19:10:59 +0900

    async 연쇄 호출 추가

--- API_Practice/API/TodosAPI+Async.swift
+++ API_Practice/API/TodosAPI+Async.swift
@@ -55,20 +55,22 @@ extension TodosAPI_Async {
         
         guard let url = URL(baseUrl: baseUrl, optionUrl: "/todos", queryItems: ["page":"\(page)"]) else {
             // eraseToAnyPublisher : Publisher wrapping
-            throw ApiError.notAllowedUrl
+            let test = ListResponse(data: nil, meta: nil, message: nil)
+            return test
         }
         
         var request = URLRequest(url: url)
         request.httpMethod = "GET"
         request.addValue("application/json", forHTTPHeaderField: "accept")
+    
+        // 리턴 앞에 throws가 있어서 do, catch 없이 사용하더라도 에러가 던져짐
+        // 단, 단순 에러를 던질 때 사용
+        // 에러를 가공해서 보내야 한다면 do, catch 를 사용해야 함
+        let listResponse = try await executeRequest(request, ListResponse.self)
         
-        do {
-            let listResponse = try await executeRequest(request, ListResponse.self)
-            return listResponse
-        } catch {
-            throw error
-        }
+        return listResponse
     }
+    
     
     static func addTodoByMultipart(content: String, isDone: Bool = false) async throws -> TodoResponse {
         
@@ -91,12 +93,9 @@ extension TodosAPI_Async {
         // 실제 데이터
         urlRequest.httpBody = form.bodyData
         
-        do {
-            let listResponse = try await executeRequest(urlRequest, TodoResponse.self)
-            return listResponse
-        } catch {
-            throw error
-        }
+        let listResponse = try await executeRequest(urlRequest, TodoResponse.self)
+        
+        return listResponse
     }
     
 //    private static func decodeResponse
@@ -124,78 +123,6 @@ extension TodosAPI_Async {
         }
     }
     
-    // failure이 있는 경우
-    // 에러를 받아서 처리하는 메서드 추가
-//    static func addTodoAndFetchTodos(content: String, isDone: Bool = false) -> AnyPublisher<BaseListResponse<Todo>, ApiError> {
-//        
-//        return self.addTodoByJson(content: content, isDone: isDone)
-//            .flatMap { _ in
-//                fetchTodos()
-//            }
-//            .mapError(checkError(err:))
-//            .eraseToAnyPublisher()
-//    }
-//    
-//    // failure이 없는 경우
-//    // 에러가 발생한 경우 내릴 값을 정해줌
-//    // flatMap : 1번 퍼블리셔에서 2번 퍼블리셔로 변경됨
-//    static func addTodoAndFetchTodosNoError(content: String, isDone: Bool = false) -> AnyPublisher<[Todo], Never> {
-//        
-//        return self.addTodoByJson(content: content, isDone: isDone)
-//            .flatMap { _ in
-//                fetchTodos()
-//            }
-//            // $0은 BaseListResponse<Todo>
-//            .compactMap({ $0.data })
-//            .replaceError(with: [])
-//            .eraseToAnyPublisher()
-//    }
-//    
-//    // SwitchToLatest
-//    // 스트림을 통해서 보내지는 값 중 마지막 값만 받고 싶을 때
-//    // switchToLatest : 가장 최근에 수신한 퍼블리셔를 받음
-//    // 헷갈린 점 : 단순 map을 통해서 내려갔으면 BaseResponse가 아닌가?
-//    // switchToLatest를 통해서 가장 최근에 수신한 퍼블리셔 BaseListResponse를 내려보냄
-//    static func addTodoAndFetchTodosNoErrorSwitchToLatest(content: String, isDone: Bool = false) -> AnyPublisher<[Todo], Never> {
-//        
-//        return self.addTodoByJson(content: content, isDone: isDone)
-//            .map { _ in
-//                fetchTodos()
-//            }
-//            .switchToLatest()
-//            .compactMap({ $0.data })
-//            .replaceError(with: [])
-//            .eraseToAnyPublisher()
-//    }
-//    
-//    static func addTodoAndFetchTodosSwitchToLatest(content: String, isDone: Bool = false) -> AnyPublisher<[Todo], ApiError> {
-//        
-//        return self.addTodoByJson(content: content, isDone: isDone)
-//            .map { _ in
-//                fetchTodos()
-//            }
-//            .switchToLatest()
-//            .compactMap({ $0.data })
-//            .mapError(checkError(err:))
-//            .eraseToAnyPublisher()
-//    }
-    
-//    static func searchTodos(searchTerm: String, page: Int = 1) -> AnyPublisher<BaseListResponse<Todo>, ApiError>{
-//        
-//        let queryItems = ["query": searchTerm, "page": "\(page)"]
-//        
-//        guard let url = URL(baseUrl: baseUrl, optionUrl: "/todos", queryItems: queryItems) else {
-//            return Fail(error: ApiError.notAllowedUrl).eraseToAnyPublisher()
-//        }
-//        
-//        var request = URLRequest(url: url)
-//        request.httpMethod = "GET"
-//        request.addValue("application/json", forHTTPHeaderField: "accept")
-//        
-//        
-//        return getBaseListResponse(request)
-//    }
-
     static func searchTodo(id: Int) async throws -> TodoResponse {
         
         guard let url = URL(string: baseUrl + "/todos" + "/\(id)") else {
@@ -206,12 +133,9 @@ extension TodosAPI_Async {
         urlRequest.httpMethod = "GET"
         urlRequest.addValue("application/json", forHTTPHeaderField: "accept")
         
-        do {
-            let listResponse = try await executeRequest(urlRequest, TodoResponse.self)
-            return listResponse
-        } catch {
-            throw error
-        }
+        let listResponse = try await executeRequest(urlRequest, TodoResponse.self)
+        
+        return listResponse
     }
     
     static func editTodoEncoded(id: Int, content: String, isDone: Bool) async throws -> TodoResponse {
@@ -228,12 +152,9 @@ extension TodosAPI_Async {
         urlRequest.addValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
         urlRequest.percentEncodeParameters(parameters: requestParams)
         
-        do {
-            let listResponse = try await executeRequest(urlRequest, TodoResponse.self)
-            return listResponse
-        } catch {
-            throw error
-        }
+        let listResponse = try await executeRequest(urlRequest, TodoResponse.self)
+        
+        return listResponse
     }
     
     static func editTodoByJson(id: Int, content: String, isDone: Bool) async throws -> TodoResponse {
@@ -270,11 +191,37 @@ extension TodosAPI_Async {
         
         urlRequest.addValue("application/json", forHTTPHeaderField: "accept")
         
+        let listResponse = try await executeRequest(urlRequest, TodoResponse.self)
+        
+        return listResponse
+    }
+    
+    // MARK: - API 연쇄 호출
+    
+    static func addTodoAndFetchTodos(content: String) async throws -> [Todo] {
+        
+        let addTodoResponse = try await addTodoByJson(content: content)
+        let fetchTodoResponse = try await fetchTodos()
+        
+        guard let todos = fetchTodoResponse.data else {
+            throw ApiError.noContent
+        }
+        
+        return todos
+    }
+    
+    static func addTodoAndFetchTodosNoError(content: String) async -> [Todo] {
+        
         do {
-            let listResponse = try await executeRequest(urlRequest, TodoResponse.self)
-            return listResponse
+            let addTodoResponse = try await addTodoByJson(content: content)
+            let fetchTodoResponse = try await fetchTodos()
+            
+            guard let todos = fetchTodoResponse.data else {
+                return []
+            }
+            return todos
         } catch {
-            throw error
+            return []
         }
     }
 
@@ -460,11 +407,11 @@ extension TodosAPI_Async {
     }
     
     private static func handleError(_ err: Error) -> ApiError {
-        if let err = err as? JSONSerialization {
+        if let _ = err as? JSONSerialization {
             return ApiError.jsonEncoding
         }
         
-        if let err = err as? DecodingError {
+        if let _ = err as? DecodingError {
             return ApiError.decodingError
         }
         
@@ -475,6 +422,7 @@ extension TodosAPI_Async {
         return ApiError.unknown(err)
     }
 }
+
 
 
 
